12f84a8503ab1804b33b6b9861f8455e1c61e43f
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-04 21:24:08 +0900

    design: not found 컴포넌트 스타일링

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -1,5 +1,36 @@
-const NotFound = () => {
-    return <>NotFound</>;
+import styled from 'styled-components';
+
+interface NotFoundPropsType {
+    error?: string;
+}
+
+const NotFound = ({error}: NotFoundPropsType) => {
+    return (
+        <NotFoundStyle>
+            <p>🚧 {error}</p>
+            <p className='sub-text'>Please try again later</p>
+        </NotFoundStyle>
+    );
 };
 
 export default NotFound;
+
+const NotFoundStyle = styled.section`
+    max-width: 768px;
+    margin-top: 150px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    p {
+        font-size: 24px;
+        line-height: 180%;
+        color: var(--gray);
+    }
+    .sub-text {
+        font-weight: 300;
+        font-size: 18px;
+    }
+`;
