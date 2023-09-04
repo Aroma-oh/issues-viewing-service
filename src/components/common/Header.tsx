1ee09017aadc260ef1fea1da2027e6ee9dea9117
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-04 19:05:57 +0900

    feat: 헤더 클릭이벤트에 스크롤 상단으로 이동 코드 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,20 @@
 // import styled, react-router-dom
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 // import constant data
 import {OWNER, REPO} from 'constants/apis';
 import {PATH} from 'constants/apis';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const moveToList = () => {
+        window.scrollTo(0, 0);
+        navigate(PATH);
+    };
+
     return (
-        <HeaderStyle to={PATH}>
+        <HeaderStyle onClick={moveToList}>
             {OWNER} / {REPO}
         </HeaderStyle>
     );
@@ -15,18 +22,17 @@ const Header = () => {
 
 export default Header;
 
-const HeaderStyle = styled(Link)`
+const HeaderStyle = styled.header`
     font-size: 32px;
     font-weight: 600;
 
     text-align: center;
-    color: black;
-    text-decoration: none;
     border-bottom: var(--border-line);
     background-color: white;
+    cursor: pointer;
 
     margin-bottom: 24px;
-    padding: 21px 0;
+    padding: 26px 0 21px 0;
     display: flex;
     justify-content: center;
 
