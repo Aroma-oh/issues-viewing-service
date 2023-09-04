02271472727fad49a48d71118fb8bf98417351ba
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-04 11:51:37 +0900

    feat: fetch data 커스텀 훅 생성

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,30 +1,32 @@
-import {useCallback, useState} from 'react';
+import {useCallback} from 'react';
 import {instance} from 'apis/instance';
 import {PATH} from 'constants/apis';
 import {AxiosError} from 'axios';
 import {UseApiType} from 'types/api';
+import {fetchIssueState} from 'recoil/atoms';
+import {useRecoilState} from 'recoil';
 
 export const useAxios = () => {
-    const [fetchDataState, setFetchDataState] = useState({
-        loading: true,
-        fetching: true,
-        error: '',
-        data: [],
-    });
+    const [fetchDataState, setFetchDataState] = useRecoilState(fetchIssueState);
 
-    const fetchData = useCallback(async (params: UseApiType) => {
-        try {
-            setFetchDataState(prev => ({...prev, fetching: true}));
-            const response = await instance.get(PATH, params);
-            setFetchDataState(prev => ({...prev, data: response.data}));
-            return response;
-        } catch (e) {
-            const error = e as AxiosError;
-            setFetchDataState(prev => ({...prev, error: error.message}));
-        } finally {
-            setFetchDataState(prev => ({...prev, loading: false, fetching: false}));
-        }
-    }, []);
+    const fetchData = useCallback(
+        async (params: UseApiType) => {
+            try {
+                setFetchDataState(prev => ({...prev, fetching: true}));
+
+                const response = await instance.get(PATH, params);
+                setFetchDataState(prev => ({...prev, data: [...prev.data, ...response.data]}));
+
+                return response;
+            } catch (e) {
+                const error = e as AxiosError;
+                setFetchDataState(prev => ({...prev, error: error.message}));
+            } finally {
+                setFetchDataState(prev => ({...prev, loading: false, fetching: false}));
+            }
+        },
+        [setFetchDataState]
+    );
 
     return {fetchData, fetchDataState};
 };
