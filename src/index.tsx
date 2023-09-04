54ca4ca6daaf0fdce7bafd272138259eb9aef0cb
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-04 17:39:55 +0900

    design: global css 코드 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import {Router} from './Router';
 import {RouterProvider} from 'react-router-dom';
-import './styles/reset.css';
+import 'styles/reset.css';
+import 'styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<RouterProvider router={Router} />);
